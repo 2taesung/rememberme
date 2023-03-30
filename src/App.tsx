e895d2b558dff9c5b5609daf5ea0309cd550d5cf
dlxotjde <dlxotjde@daum.net>
2023-03-30 23:56:06 +0900

    feat: GlobalStyle

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Reset } from 'styled-reset';
 import MapSection from './components/MapSection';
+import GlobalStyle from './styles/GlobalStyle';
 
 export default function App() {
   return (
     <>
       <Reset />
+      <GlobalStyle />
       <p>Hello</p>
       <MapSection />
     </>
