5269275fc508cbd00d0a6c7b70d96a8cd7552dc9
dlxotjde <dlxotjde@daum.net>
2023-03-30 23:41:57 +0900

    chore: npm styled-reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { Reset } from 'styled-reset';
 import MapSection from './components/MapSection';
 
 export default function App() {
   return (
     <>
+      <Reset />
       <p>Hello</p>
       <MapSection />
     </>
