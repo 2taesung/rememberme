8f235cb2ea198212d00f094975fa83161d4ffff8
dlxotjde <dlxotjde@daum.net>
2023-04-08 10:26:45 +0900

    Chore: Lottiefile && typescript

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,20 @@
+import Lottie from 'react-lottie';
+import animationData from '../lotties/kiss-of-the-heart.json';
+
 export default function Navbar() {
-  return <>Navbar</>;
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
+
+  return (
+    <>
+      <div>Navbar</div>
+      <Lottie options={defaultOptions} height={400} width={400} />
+    </>
+  );
 }
