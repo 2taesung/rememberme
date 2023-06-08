4c88b242e1216b564d9dd6039734e3c8fc4f493f
root <root@DESKTOP-J22D89D>
2023-06-09 00:49:19 +0900

    remove: react-lotties

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,20 @@
-import Lottie from 'react-lottie';
-import animationData from '../lotties/kiss-of-the-heart.json';
+// import Lottie from 'react-lottie';
+// import animationData from '../lotties/kiss-of-the-heart.json';
 
 export default function Navbar() {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animationData,
-    rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice',
-    },
-  };
+  // const defaultOptions = {
+  //   loop: true,
+  //   autoplay: true,
+  //   animationData: animationData,
+  //   rendererSettings: {
+  //     preserveAspectRatio: 'xMidYMid slice',
+  //   },
+  // };
 
   return (
     <>
       <div>Navbar</div>
-      <Lottie options={defaultOptions} height={400} width={400} />
+      {/* <Lottie options={defaultOptions} height={400} width={400} /> */}
     </>
   );
 }
