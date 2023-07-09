637ce0d007097ff9435aa78120ea24f52cbeef98
root <root@DESKTOP-J22D89D>
2023-07-10 00:45:54 +0900

    add: list components

---
+++ src/mainPage/AllList.tsx
@@ -0,0 +1,53 @@
+import ListSubheader from '@mui/material/ListSubheader';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import Collapse from '@mui/material/Collapse';
+import ExpandLess from '@mui/icons-material/ExpandLess';
+import ExpandMore from '@mui/icons-material/ExpandMore';
+import StarBorder from '@mui/icons-material/StarBorder';
+import { useState } from 'react';
+import { Radar } from '@mui/icons-material';
+import PositionList from './PositionList';
+
+export default function AllList() {
+  const [open, setOpen] = useState(true);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <List
+      sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
+      component="nav"
+      aria-labelledby="nested-list-subheader"
+      subheader={
+        // eslint-disable-next-line react/jsx-wrap-multilines
+        <ListSubheader component="div" id="nested-list-subheader">
+          Nested List Items
+        </ListSubheader>
+      }
+    >
+      <ListItemButton onClick={handleClick}>
+        <ListItemIcon>
+          <Radar />
+        </ListItemIcon>
+        <ListItemText primary="Position" />
+        {open ? <ExpandLess /> : <ExpandMore />}
+      </ListItemButton>
+      <Collapse in={open} timeout="auto" unmountOnExit>
+        <List component="div" disablePadding>
+          <ListItemButton sx={{ pl: 4 }}>
+            <ListItemIcon>
+              <StarBorder />
+            </ListItemIcon>
+            <PositionList />
+          </ListItemButton>
+        </List>
+      </Collapse>
+    </List>
+  );
+}
--- src/mainPage/PositionList.tsx
+++ src/mainPage/PositionList.tsx
@@ -1,15 +1,38 @@
+import { List, ListItem, ListItemText } from '@mui/material';
+import { cloneElement } from 'react';
 import usePostionListStore from '../hooks/usePositionListStore';
 
 export default function PositionList() {
   const [snapshot] = usePostionListStore();
   const { positions } = snapshot;
 
+  function generate(element: React.ReactElement) {
+    return [0].map(
+      value =>
+        // eslint-disable-next-line implicit-arrow-linebreak
+        cloneElement(element, {
+          key: value,
+        }),
+      // eslint-disable-next-line function-paren-newline
+    );
+  }
+
   return (
-    <>
-      <h3>PositionList</h3>
-      {
-        positions.map(position => (<p key={position.id}>{position.title}</p>))
-      }
-    </>
+    // <>
+    //   <h3>PositionList</h3>
+    //   {
+    //     positions.map(position => (<p key={position.id}>{position.title}</p>))
+    //   }
+    // </>
+    <List>
+      {generate(
+        <ListItem>
+          <ListItemText
+            primary="Single-line item"
+            // secondary={secondary ? 'Secondary text' : null}
+          />
+        </ListItem>,
+      )}
+    </List>
   );
 }
