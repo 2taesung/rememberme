6ca9ec0e654b11d6b919f07b9ab7c0f84a24779a
root <root@DESKTOP-J22D89D>
2023-07-12 04:04:29 +0900

    add: mock data id 2

--- fixtures/positions.ts
+++ fixtures/positions.ts
@@ -1,12 +1,20 @@
 import { TPosition } from '../src/types/Position';
 
-const positions: TPosition[] = [{
-  id: 1,
-  lat: 33.55635,
-  lng: 126.795841,
-  title: '1',
-  contents: 'contents',
-},
+const positions: TPosition[] = [
+  {
+    id: 1,
+    lat: 33.55635,
+    lng: 126.795841,
+    title: '1번 타이틀',
+    contents: 'contents',
+  },
+  {
+    id: 2,
+    lat: 33.55635,
+    lng: 126.795841,
+    title: '2번 타이틀',
+    contents: 'contents',
+  },
 ];
 
 export default positions;
