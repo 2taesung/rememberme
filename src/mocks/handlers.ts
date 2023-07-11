f94cbb8bbab7a6a9e7c34d958f4c6fb16501463c
root <root@DESKTOP-J22D89D>
2023-07-12 04:02:28 +0900

    refactor: MSW browser

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,13 +1,14 @@
-// import { rest } from 'msw';
-// import { ProductType } from '../types';
-// import fixtures from '../../fixtures';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { rest } from 'msw';
+import fixtures from '../../fixtures';
+import { TPosition } from '../types/Position';
 
-// export const handlers = [
-//   rest.get('/products/:id', (req, res, ctx) => {
-//     const { id } = req.params;
-//     const filtered = fixtures.products.filter(
-//       (product: ProductType) => product.id === Number(id),
-//     );
-//     return res(ctx.status(200), ctx.json({ data: { product: filtered[0] } }));
-//   }),
-// ];
+export const handlers = [
+  rest.get('/test/position/:id', (req, res, ctx) => {
+    const { id } = req.params;
+    const filtered = fixtures.positions.filter(
+      (position: TPosition) => position.id === Number(id),
+    );
+    return res(ctx.json({ data: { position: filtered[0] } }));
+  }),
+];
