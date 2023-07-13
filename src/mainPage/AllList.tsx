88837649be24406a48462560403ffb39357e9def
root <root@DESKTOP-J22D89D>
2023-07-13 09:12:11 +0900

    remove: subtitle

--- src/mainPage/AllList.tsx
+++ src/mainPage/AllList.tsx
@@ -1,13 +1,10 @@
-import ListSubheader from '@mui/material/ListSubheader';
 import List from '@mui/material/List';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import Collapse from '@mui/material/Collapse';
 import ExpandLess from '@mui/icons-material/ExpandLess';
 import ExpandMore from '@mui/icons-material/ExpandMore';
-import StarBorder from '@mui/icons-material/StarBorder';
 import { useState } from 'react';
 import { Radar } from '@mui/icons-material';
 import PositionList from './PositionList';
@@ -20,17 +17,7 @@ export default function AllList() {
   };
 
   return (
-    <List
-      sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
-      component="nav"
-      aria-labelledby="nested-list-subheader"
-      subheader={
-        // eslint-disable-next-line react/jsx-wrap-multilines
-        <ListSubheader component="div" id="nested-list-subheader">
-          Nested List Items
-        </ListSubheader>
-      }
-    >
+    <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
       <ListItemButton onClick={handleClick}>
         <ListItemIcon>
           <Radar />
@@ -39,14 +26,7 @@ export default function AllList() {
         {open ? <ExpandLess /> : <ExpandMore />}
       </ListItemButton>
       <Collapse in={open} timeout="auto" unmountOnExit>
-        <List component="div" disablePadding>
-          <ListItemButton sx={{ pl: 4 }}>
-            <ListItemIcon>
-              <StarBorder />
-            </ListItemIcon>
-            <PositionList />
-          </ListItemButton>
-        </List>
+        <PositionList />
       </Collapse>
     </List>
   );
