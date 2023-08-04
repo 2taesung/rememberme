f1aeaa860672143204e21b479cbb82b9d7b3a16a
root <root@DESKTOP-J22D89D>
2023-08-04 16:17:55 +0900

    Add: time fixtures data

---
+++ fixtures/times.ts
@@ -0,0 +1,16 @@
+import { TTime } from '../src/types';
+
+export const times: TTime[] = [
+  {
+    id: 1,
+    ts: '2023-08-04T05: 37:00.931Z',
+    title: '1번 타이틀',
+    contents: 'contents',
+  },
+  {
+    id: 2,
+    ts: '2023-08-04T05:37:24.793Z',
+    title: '2번 타이틀',
+    contents: 'contents',
+  },
+];
