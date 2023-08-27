8adb2e36bdf3a8a894e098cc23f38facb72643e8
root <root@DESKTOP-J22D89D>
2023-08-27 20:19:16 +0900

    refactor: real title

--- fixtures/positions.ts
+++ fixtures/positions.ts
@@ -5,14 +5,14 @@ export const positions: TPosition[] = [
     id: 1,
     lat: 33.55635,
     lng: 126.795841,
-    title: '1번 타이틀',
+    title: '실행 컨텍스트는 무엇인가?',
     contents: 'contents',
   },
   {
     id: 2,
     lat: 33.55635,
     lng: 126.795841,
-    title: '2번 타이틀',
+    title: '클로저란 무엇인가?',
     contents: 'contents',
   },
 ];
