7f203a3818fbc396e2eaa4b83ab3082458762a90
root <root@DESKTOP-J22D89D>
2023-09-04 23:56:53 +0900

    feat: time list view in home

--- src/mainPage/AllList.tsx
+++ src/mainPage/AllList.tsx
@@ -6,14 +6,18 @@ import Collapse from '@mui/material/Collapse';
 import ExpandLess from '@mui/icons-material/ExpandLess';
 import ExpandMore from '@mui/icons-material/ExpandMore';
 import { useState } from 'react';
-import { Radar } from '@mui/icons-material';
+import { Radar, AccessTime } from '@mui/icons-material';
 import PositionList from './PositionList';
 
 export default function AllList() {
   const [positionListOpen, setPositionListOpen] = useState(true);
+  const [timeListOpen, setTimeListOpen] = useState(true);
 
   const positionHandleClick = () => {
     setPositionListOpen(!positionListOpen);
+  };
+  const timeHandleClick = () => {
+    setTimeListOpen(!timeListOpen);
   };
 
   return (
@@ -26,6 +30,16 @@ export default function AllList() {
         {positionListOpen ? <ExpandLess /> : <ExpandMore />}
       </ListItemButton>
       <Collapse in={positionListOpen} timeout="auto" unmountOnExit>
+        <PositionList />
+      </Collapse>
+      <ListItemButton onClick={timeHandleClick}>
+        <ListItemIcon>
+          <AccessTime />
+        </ListItemIcon>
+        <ListItemText primary="Time" />
+        {timeListOpen ? <ExpandLess /> : <ExpandMore />}
+      </ListItemButton>
+      <Collapse in={timeListOpen} timeout="auto" unmountOnExit>
         <PositionList />
       </Collapse>
     </List>
